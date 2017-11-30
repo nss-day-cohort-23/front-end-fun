7bd5b1a191b6ebcd186b898591b677229caf56d2
joechesney <33613119+joechesney@users.noreply.github.com>
2017-11-30 15:17:33 -0600

    finished project

--- ms1-review/main.js
+++ ms1-review/main.js
@@ -1,85 +1,70 @@
 console.log("ms1 review");
-
-let dogContent = document.getElementById("dogs");
-let rainbowContent = document.getElementById("rainbows");
-let dogBtn = document.getElementById("dog");
-let rainbowBtn = document.getElementById("rainbow");
-let dogH1 = document.getElementById("dogH1");
-let rainbowH1 = document.getElementById("rainbowH1");
+const dogsContent = document.getElementById("dogs");
+const rainbowContent = document.getElementById("rainbows");
 
 let dogPics = {
-  pics: [
-    { url: "img/dog1.jpeg", title: "Happy Dog", cat: "dog" },
-    { url: "img/dog2.jpeg", title: "Fuzzy Dog", cat: "dog" },
-    { url: "img/dog3.jpeg", title: "Barking Dog", cat: "dog" }
-  ]
-};
+    pics: [
+        { url: "img/dog1.jpeg", title: "Happy dog", cat: "rainbow" },
+        { url: "img/dog2.jpeg", title: "Fuzzy dog", cat: "rainbow"},
+        { url: "img/dog3.jpeg", title: "Barking dog", cat: "rainbow" }
+    ]
+}
+let bowPics= {
+    pics: [
+        { url: "img/rainbow1.jpeg", title: "Happy rainbow", cat: "dog" },
+        { url: "img/rainbow2.jpeg", title: "Fuzzy rainbow", cat: "dog" },
+        { url: "img/rainbow3.jpeg", title: "Barking rainbow", cat: "dog" }
+    ]
+}
+// We want to dynamically create picture elements and give them pics and classes
+// make n sections: n is based on the amount of images
+// insert pictures
 
-let bowPics = {
-  pics: [
-    { url: "img/rainbow1.jpeg", title: "Happy rainbow", cat: "rainbow" },
-    { url: "img/rainbow2.jpeg", title: "Fuzzy rainbow", cat: "rainbow" },
-    { url: "img/rainbow3.jpeg", title: "Barking rainbow", cat: "rainbow" }
-  ]
-};
+//loop through the images and create sections one at a time
 
-// dynamically create some elements to insert pics into
-// make n sections: n is based on number of imgs are available
-// insert the pictures
-// loop through the images and create sections one at a time
-
-for (let i = 0; i < dogPics.pics.length; i++) {
-  let section = document.createElement("section");
-  section.setAttribute("class", "dog-card card");
-  section.style.backgroundImage = `url(${dogPics.pics[i].url})`;
-  dogContent.appendChild(section);
+for (let i = 0; i <dogPics.pics.length; i++){
+    let section = document.createElement("section");
+    section.setAttribute("class", "dog-card card");
+    section.style.backgroundImage = `url(${dogPics.pics[i].url})`;
+    dogsContent.appendChild(section);
 }
 
 for (let i = 0; i < bowPics.pics.length; i++) {
-  let section = document.createElement("section");
-  section.setAttribute("class", "rainbow-card card");
-  section.style.backgroundImage = `url(${bowPics.pics[i].url})`;
-  rainbowContent.appendChild(section);
+    let section = document.createElement("section");
+    section.setAttribute("class", "rainbow-card card");
+    section.style.backgroundImage = `url(${bowPics.pics[i].url})`;
+    rainbowContent.appendChild(section);
 }
 
-function updateCard(cards, category) {
-  console.log("cards", cards);
+// click on a button and make the pics swap out for something else
+const monkeyfyButton = document.getElementById("monkeyfy");
+const allDogCards = document.getElementsByClassName("dog-card");
+const dogH1 = document.getElementById("dogH1");
 
-  for (let i = 0; i < cards.length; i++) {
-    // image change and border change
-    cards[i].classList.toggle(`fancy-border`);
-    cards[i].classList.toggle(`${category}-fied`);
-  }
+monkeyfyButton.addEventListener("click", function(){
+    updateCard(allDogCards, "monkey");
+    updateText(monkeyfyButton, dogH1, "Monkey", "Dog");
+});
+const horsefyButton = document.getElementById("horsefy");
+const allHorseCards = document.getElementsByClassName("rainbow-card");
+const rainbowH1 = document.getElementById("rainbowH1");
+horsefyButton.addEventListener("click", function () {
+        updateCard(allHorseCards, "horse");
+        updateText(horsefyButton, rainbowH1, "Rambow", "Rainbow");
+});
+function updateCard(cards, category){
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].classList.toggle(`fancy-border`); // border change
+        cards[i].classList.toggle(`${category}-fied`); //image change
+    }
 }
-
-function updateText(btn, h1, cat1, cat2) {
-  // h1 text change and button text change
-  btn.classList.toggle(cat1);
-  if (btn.classList.contains(cat1)) {
-    console.log(this);
-    event.target.innerHTML = `${cat2}ify It!`;
-    h1.innerHTML = cat1;
-  } else {
-    event.target.innerHTML = `${cat1}fy It!`;
-    h1.innerHTML = cat2;
-  }
-}
-
-// Click on a button and make the pictures swap out with something else
-// event listener on each button
-let buttons = document.getElementsByTagName("button");
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function() {
-    console.log(event.target.id, event.target.getAttribute("data-pairId"));
-    updateCard(
-      document.getElementsByClassName(`${event.target.id}-card`),
-      event.target.getAttribute("data-pairId")
-    );
-    updateText(
-      event.target,
-      `${event.target.id}H1`,
-      event.target.getAttribute("data-pairId"),
-      event.target.id
-    );
-  });
+function updateText(btn, h1, cat1, cat2){
+    btn.classList.toggle(cat1);
+    if (btn.classList.contains(cat1)) {
+        event.target.innerHTML = `${cat2}ify It!`; // button text change
+        h1.innerHTML = cat1; // h1 text change
+    } else {
+        this.innerHTML = `${cat1}ify It!`; // button text change
+        h1.innerHTML = cat2;// h1 text change
+    }
 }
